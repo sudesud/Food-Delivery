9bdf1f047254dab3c3fd87fdadfa85be9ef2526a
sude tacer <112017756+sudesud@users.noreply.github.com>
2025-05-14 14:09:13 +0300

    adminpaneliyapildi

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,16 @@
+import logo from './logo.png';
+import admin_profile from './admin_profile.png'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import upload_image from './upload-image.png'
+
+
+
+
+export const assets={
+    logo,
+    admin_profile,
+    add_icon,
+    order_icon,
+    upload_image
+}
---
+++ admin/src/pages/Orders/Orders.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Orders.css'
+const Order = () => {
+  return (
+    <div>
+      order
+    </div>
+  )
+}
+
+export default Order
